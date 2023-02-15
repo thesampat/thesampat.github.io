1a0954dd1406f8813ecf982dd81f2775e4ca214f
thesampat <92632926+thesampat@users.noreply.github.com>
2023-02-15 15:12:52 +0530

    update1

--- translate.mjs
+++ translate.mjs
@@ -1,27 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDFMTU1C1OtR1a3JpADig5IHKhbDhV3oC8",
-  authDomain: "codingallstarts.firebaseapp.com",
-  projectId: "codingallstarts",
-  storageBucket: "codingallstarts.appspot.com",
-  messagingSenderId: "736510337410",
-  appId: "1:736510337410:web:bfb76976daffba9643ef9b",
-  measurementId: "G-W4QRZZ2096"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-
 const KEY = 'AIzaSyAoJwn2tmtL-xN3acUyTCyq2dX40ohLIqI'
 const language = 'HI'
 const string = "Helo how are you"
